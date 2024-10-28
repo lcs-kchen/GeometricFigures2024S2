197d3dfbb89a9489944f549cd0efb2cb5d7cd7f0
Kevin Chen <kchen@lcs.on.ca>
2024-10-28 17:48:48 -0400

    Initial Commit

---
+++ GeometricFigures1/GeometricFigures1App.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricFigures1App.swift
+//  GeometricFigures1
+//
+//  Created by Haowen Chen on 2024-10-28.
+//
+
+import SwiftUI
+
+@main
+struct GeometricFigures1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
